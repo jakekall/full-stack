0ad9a44a20e1cd7514f283cefabbb3084b436f2b
Jake Kallioniemi <37346883+JakeKallioniemi@users.noreply.github.com>
2020-12-09 23:31:10 +0200

    8.17-8.18

--- part_8/Books/front/src/App.js
+++ part_8/Books/front/src/App.js
@@ -1,21 +1,39 @@
 import React, { useState } from 'react'
+import { useApolloClient } from '@apollo/client'
 import Authors from './components/Authors'
 import Books from './components/Books'
 import NewBook from './components/NewBook'
+import LoginForm from './components/LoginForm'
+import RegisterForm from './components/RegisterForm'
 
 const App = () => {
   const [page, setPage] = useState('authors')
+  const [token, setToken] = useState(null)
+  const client = useApolloClient()
+
+  const logout = () => {
+    setToken(null)
+    localStorage.clear()
+    client.resetStore()
+  }
 
   return (
     <div>
       <div>
         <button onClick={() => setPage('authors')}>authors</button>
         <button onClick={() => setPage('books')}>books</button>
-        <button onClick={() => setPage('add')}>add book</button>
+        {token && <button onClick={() => setPage('add')}>add book</button>}
+        {token && <button onClick={logout}>logout</button>}
+        {!token && <button onClick={() => setPage('login')}>login</button>}
+        {!token && (
+          <button onClick={() => setPage('register')}>register</button>
+        )}
       </div>
       <Authors show={page === 'authors'} />
       <Books show={page === 'books'} />
       <NewBook show={page === 'add'} />
+      <LoginForm show={page === 'login'} setToken={setToken} />
+      <RegisterForm show={page === 'register'} />
     </div>
   )
 }
---
+++ part_8/Books/front/src/components/RegisterForm.js
@@ -0,0 +1,47 @@
+import React, { useState } from 'react'
+import { useMutation } from '@apollo/client'
+import { CREATE_USER } from '../queries'
+
+const RegisterForm = ({ show }) => {
+  const [username, setUsername] = useState('')
+  const [favoriteGenre, setfavoriteGenre] = useState('')
+
+  const [register] = useMutation(CREATE_USER)
+
+  const submit = async (event) => {
+    event.preventDefault()
+
+    register({
+      variables: { username, favoriteGenre },
+    })
+  }
+
+  if (!show) {
+    return null
+  }
+
+  return (
+    <div>
+      <form onSubmit={submit}>
+        <div>
+          username{' '}
+          <input
+            value={username}
+            onChange={({ target }) => setUsername(target.value)}
+          />
+        </div>
+        <div>
+          favorite genre{' '}
+          <input
+            type="favorite genre"
+            value={favoriteGenre}
+            onChange={({ target }) => setfavoriteGenre(target.value)}
+          />
+        </div>
+        <button type="submit">register</button>
+      </form>
+    </div>
+  )
+}
+
+export default RegisterForm
