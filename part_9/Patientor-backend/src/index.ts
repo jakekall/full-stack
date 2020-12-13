b383e4c57378d18d2f9c06c67932a625eeabb383
Jake Kallioniemi <37346883+JakeKallioniemi@users.noreply.github.com>
2020-12-13 17:05:10 +0200

    9.9-9.11

--- part_9/Patientor-backend/src/index.ts
+++ part_9/Patientor-backend/src/index.ts
@@ -1,13 +1,26 @@
 import express from 'express';
+import cors from 'cors';
+import diagnoseRouter from './routes/diagnoses';
+import patientsRouter from './routes/patients';
+
 const app = express();
 app.use(express.json());
 
-const PORT = 3000;
+// There's some type issue that is discussed for example here https://github.com/DefinitelyTyped/DefinitelyTyped/issues/43909
+// I couldn't get it to work by changing package versions so I'm using this instead from the same issue
+app.use((_req, _res, next) => {
+  next();
+}, cors());
 
-app.get('/ping', (_req, res) => {
+const PORT = 3001;
+
+app.get('/api/ping', (_req, res) => {
   console.log('someone pinged here');
   res.send('pong');
 });
+
+app.use('/api/patients', patientsRouter);
+app.use('/api/diagnoses', diagnoseRouter);
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
